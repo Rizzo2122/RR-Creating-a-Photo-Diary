61915af9fdd7de0f63ea3b1448e9a8ef8e0b94de
Dakota Greenlee <dg2120032@gmail.com>
2023-02-01 19:44:35 -0800

    fixed bin.js

--- bin.js
+++ bin.js
@@ -2,7 +2,7 @@
 const open = require('open')
 const { exec, spawn } = require('child_process')
 exec('npx webpack --mode="development"', () => {
-    spawn('npx', [ 'webpack', '--watch', '--mode="development"' ], { stdio: 'inherit' });
+    spawn('npx', ['webpack', '--watch', '--mode="development"'], { stdio: 'inherit', shell: process.platform == 'win32' });
     open('./public/index.html')
     console.log('Watching for changes in "./src"')
 })
